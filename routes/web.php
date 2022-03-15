8b50a114c3c0e34019c3df52d6f41d52e72c9f0f
Vasiliy <kkolyasev1228@gmail.com>
2022-03-15 17:00:00 +0400

    delete my registrations

--- routes/web.php
+++ routes/web.php
@@ -1,38 +1,9 @@
 <?php
 
-use Illuminate\Foundation\Auth\EmailVerificationRequest;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
-})->name('home');
-
-//Auth
-Route::group(['middleware' => 'guest'], function () {
-    Route::get('/register', 'UserController@register')->name('register.create');
-    Route::post('/register', 'UserController@store')->name('register.store');
-    Route::get('/login', 'UserController@loginForm')->name('login.create');
-    Route::post('/login', 'UserController@login')->name('login');
 });
-Route::get('/logout', 'UserController@logout')->name('logout')->middleware('auth');
 
-Route::get('/email/verify', function () {
-    return view('auth.verify-email');
-})->middleware('auth')->name('verification.notice');
-
-Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
-    $request->fulfill();
-
-    return redirect()->route('home');
-})->middleware(['auth', 'signed'])->name('verification.verify');
